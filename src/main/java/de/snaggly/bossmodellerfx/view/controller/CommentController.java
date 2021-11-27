6e7c5c8b0e6b078b246b1a1d0666f319e462c968
Snaggly <oemshani@yahoo.com>
2021-11-27 23:45:59 +0100

    Bugfix: Comment model does not get new text data

--- src/main/java/de/snaggly/bossmodellerfx/view/controller/CommentController.java
+++ src/main/java/de/snaggly/bossmodellerfx/view/controller/CommentController.java
@@ -1,24 +1,31 @@
 package de.snaggly.bossmodellerfx.view.controller;
 
 import de.snaggly.bossmodellerfx.model.Comment;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 
 public class CommentController implements ViewController<Comment> {
     private boolean isEditable = false;
+    private Comment model;
 
     @FXML
     TextArea commentTextArea;
 
     @Override
     public void loadModel(Comment model) {
+        this.model = model;
         commentTextArea.setText(model.getName());
     }
 
     @FXML
     private void initialize(){
-
+        commentTextArea.textProperty().addListener((observableValue, s, newText) -> {
+            if (model != null)
+                model.setName(newText);
+        });
     }
 
     @FXML
